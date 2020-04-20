8c5eba8d4ccd907518a33be4d8b11af576759e42
Scott Mosher <scott@scottmosherphotography.com>
2020-04-20 15:58:12 -0600

    Working on next assignment

--- web/index.php
+++ web/index.php
@@ -1,28 +1,11 @@
 <?php
 
-  // @include_once('common/header.php');
-  // @include_once('common/nav.php');
+  @include_once('common/header.php');
+  @include_once('common/nav.php');
 
 ?>
 
-<!DOCTYPE html>
-<html  lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="description" content="Home">
-        <meta name="author" content="Scott Mosher">
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-    
-        <title>Home</title>
-    </head>
 
-    
-    <header>
-        <div class="leftContent">
-            <h2>Trails for Bikers</h2>
-        </div>
-
-    </header>   
 <body>
 
 
