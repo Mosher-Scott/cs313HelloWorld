ff25e5b7e27353eb976f6321a3d1c679662210dc
Scott Mosher <scott@scottmosherphotography.com>
2020-04-20 16:08:43 -0600

    Modified header and index

--- web/common/header.php
+++ web/common/header.php
@@ -5,7 +5,7 @@
         <meta name="description" content="Home">
         <meta name="author" content="Scott Mosher">
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="../css/main.css">
 
         <title>CS 313 - Home</title>
     </head>
