26650d1c15fb8946af6f6cc045a16829ac3391c4
Scott Mosher <scott@scottmosherphotography.com>
2020-04-20 16:06:03 -0600

    Modifed Nav Links

--- web/common/header.php
+++ web/common/header.php
@@ -7,13 +7,13 @@
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link rel="stylesheet" href="css/main.css">
 
-        <title>Home</title>
+        <title>CS 313 - Home</title>
     </head>
 
     
     <header>
         <div class="leftContent">
-            <h2>Trails for Bikers</h2>
+            <h2>CS 313 - Backend Development</h2>
         </div>
 
     </header>   
--- web/common/nav.php
+++ web/common/nav.php
@@ -2,8 +2,8 @@
     <label for="toggle">Menu</label>
     <input id="toggle" type="checkbox">
     <ul id="menu">
-        <li><a href="#">Home</a></li>
-        <li><a href="aboutUs">Assignments</a></li>
+        <li><a href="../index.php">Home</a></li>
+        <li><a href="../pages/assignments.php">Assignments</a></li>
     </ul>
 </nav>
     
