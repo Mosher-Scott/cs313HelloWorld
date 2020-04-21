3eb952b4c0dcb091048a75dc484bf16d3438165a
Scott Mosher <scott@scottmosherphotography.com>
2020-04-21 14:42:44 -0600

    Images added

--- web/common/footer.php
+++ web/common/footer.php
@@ -1,10 +1,13 @@
         <footer>
             <div>
-            <?php 
+            <p><?php 
                 echo "Today's date is: "; 
                 $today = date("m/d/Y"); 
                 echo $today; 
-                ?> 
+                ?> </p>
+            <p><?php
+                echo "You are currently using {$_SERVER['HTTP_USER_AGENT']} to view this page";
+            ?>
             </div>
         </footer>
     </body>
