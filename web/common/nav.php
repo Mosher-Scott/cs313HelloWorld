f8d2683d1e7293319f94d754adc89f13d8aec4c8
Scott Mosher <scott@scottmosherphotography.com>
2020-04-20 18:52:45 -0600

    Tweaked CSS

--- web/common/nav.php
+++ web/common/nav.php
@@ -3,6 +3,6 @@
     <input id="toggle" type="checkbox">
     <ul id="menu">
         <li class=" btn btn-primary"><a href="../index.php">Home</a></li>
-        <li><a href="../pages/assignments.php">Assignments</a></li>
+        <li class=" btn btn-primary"><a href="../pages/assignments.php">Assignments</a></li>
     </ul>
 </nav>
