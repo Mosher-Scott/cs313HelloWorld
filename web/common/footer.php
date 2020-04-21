10c85575a37e015491f150e95a3b3a0d8b85a8e7
Scott Mosher <scott@scottmosherphotography.com>
2020-04-21 14:46:05 -0600

    Tweaked footer

--- web/common/footer.php
+++ web/common/footer.php
@@ -1,14 +1,21 @@
         <footer>
-            <div>
+            <
+    <div class="container-fluid no padding">
+      <div class="row">
+        <div class="col">
             <p><?php 
                 echo "Today's date is: "; 
                 $today = date("m/d/Y"); 
                 echo $today; 
-                ?> </p>
-            <p><?php
-                echo "You are currently using {$_SERVER['HTTP_USER_AGENT']} to view this page";
-            ?>
-            </div>
+                ?> 
+            </p>
+        </div>
+        <div class="col">
+            <p>Copyright 2020</p>
+        </div>
+      </div>
+
+                
         </footer>
     </body>
 </html>
