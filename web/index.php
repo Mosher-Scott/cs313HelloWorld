d3b16d5aade20dbbbf768bd0f211085dbb8935b2
Scott Mosher <scott@scottmosherphotography.com>
2020-04-20 18:26:09 -0600

    Added portrait

--- web/index.php
+++ web/index.php
@@ -7,20 +7,28 @@
 
 
 <body>
-  <div>
-    <h1>About Me</h1>
-  </div>
-  <div class="container">
-            <div class="row">
-                <div class="col-sm">
-                    <h3>CS 313 - Backend Development</h3>
-                    <p>My name is Scott, and this is my website for CS 313.
-                </div>
-                <div class="col-sm">
-                    <h3>Hobbies</h3>
-                </div>
-            </div>
-            
-        </div>
+  <main>
+    <section>
+      <div>
+        <h1>About Me</h1>
+      </div>
+    </section>
 
+    <section>
+      <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <h3>CS 313 - Backend Development</h3>
+                <p>My name is Scott, and this is my website for CS 313.
+            </div>
+            <div class="col-med">
+                <img src="../images/me.jpg" alt="Picture of Scott">
+            </div>
+            <div class="col-sm">
+                <h3>Hobbies</h3>
+            </div>
+        </div>  
+      </div>
+    </section>
+  </main>
 </body>
