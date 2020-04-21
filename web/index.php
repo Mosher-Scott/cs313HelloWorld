737ce3c122d819013d592777fb447b65aaea85d7
Scott Mosher <scott@scottmosherphotography.com>
2020-04-21 14:31:48 -0600

    Images added

--- web/index.php
+++ web/index.php
@@ -69,9 +69,12 @@
         </div>
       </div>
 
+      <br>
       <div class="row">
         <div class="col">
+          <h4>Mountain Biking</h4>
           <p>Video games is something else I love.  I'm slowly starting a collection of old video games and systems, mainly the ones I grew up playing.</p>
+          <p>I have an Atari 2600 that used to be at my grandmothers house. I've also got the N64 still from high school, and I've also collected a PS2, Xbox 360, Wii, and I've built up a Raspberry Pi with a ton of different games in it.</p>
         </div>
         <div class="col">
           <img src="../images/atari.jpg" alt="Some old video game systems" class="img-rounded">
