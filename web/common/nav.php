674e81c1c237342a87bc068f91a885c11331b46c
Scott Mosher <scott@scottmosherphotography.com>
2020-04-20 19:34:51 -0600

    Tweaked CSS

--- web/common/nav.php
+++ web/common/nav.php
@@ -1,4 +1,9 @@
-<nav>
+<div class="container-fluid">
+    <div class="col">
+    <h2>CS 313 - Backend Development</h2>
+    </div>
+    <div class="col">
+    <nav>
     <label for="toggle">Menu</label>
     <input id="toggle" type="checkbox">
     <ul id="menu">
@@ -6,3 +11,5 @@
         <li class=" btn btn-primary"><a href="../pages/assignments.php">Assignments</a></li>
     </ul>
 </nav>
+    </div>
+</div>
