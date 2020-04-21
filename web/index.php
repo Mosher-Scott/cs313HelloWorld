ecfd96c0644ee6113ca7b83c5afdb35582f3afaa
Scott Mosher <scott@scottmosherphotography.com>
2020-04-20 19:09:01 -0600

    Tweaked CSS

--- web/index.php
+++ web/index.php
@@ -39,3 +39,13 @@
   <div class="col">col</div>
 </div>
   </main>
+
+  <div class="container-fluid">
+  <h2>Three Equal Columns</h2>
+  <p>Use the .col class on a specified number of elements and Bootstrap will recognize how many elements there are (and create equal-width columns). In the example below, we use three col elements, which gets a width of 33.33% each.</p>
+  <div class="row">
+    <div class="col bg-success">.col</div>
+    <div class="col bg-warning">.col</div>
+    <div class="col bg-success">.col</div>
+  </div>
+</div>
