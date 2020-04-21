f2cf4997d39736345f0a57ffcc0f54bfc9e1c9ea
Scott Mosher <scott@scottmosherphotography.com>
2020-04-20 19:59:38 -0600

    Tweaked CSS

--- web/common/footer.php
+++ web/common/footer.php
@@ -1,11 +1,11 @@
         <footer>
-        <?php 
-  
-  echo "Today's date is: "; 
-  $today = date("m/d/Y"); 
-  echo $today; 
-    
-  ?> 
+            <div>
+            <?php 
+                echo "Today's date is: "; 
+                $today = date("m/d/Y"); 
+                echo $today; 
+                ?> 
+            </div>
         </footer>
     </body>
 </html>
