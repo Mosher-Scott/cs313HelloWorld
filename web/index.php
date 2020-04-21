cdc5069490f693fadd64a5da4ffa6c6a2ca28441
Scott Mosher <scott@scottmosherphotography.com>
2020-04-20 20:09:05 -0600

    Tweaked CSS

--- web/index.php
+++ web/index.php
@@ -15,6 +15,7 @@
             <div class="col">
                 <p>My name is Scott, and this is my website for CS 313.  I am a Senior at BYU Idaho with a major of Web Development, with an emphasis on backend development.<p>
                 <p>During my time at BYU-I I have studied C++, HTMl, CSS, PHP, SQL, C#, Entity Framework, and the MVC design patterns.  I currently have earned certificates in Software Programming, Computer Information Technology, and Web Backend Development.  I also have earned an AS degree in Computer Information Technology.</p>
+                <p>I currently live in Utah with my family and work for a high tech company.</p>
             </div>
             <div class="col">
                 <img src="../images/me.jpg" alt="Picture of Scott">
@@ -27,7 +28,16 @@
             <div class="col">
               <h3>Hobbies</h3>
             </div>    
-        </div>
+      </div>
+    <div class="row">
+      <div class="col">
+        <img src="../images/weddings.jpg" alt="Wedding photo by Scott Mosher Photography">
+      </div>
+      <div class="col">
+      <p>Before I moved to Utah, I professionally photographed weddings and families.  I've been able to photograph over 100 weddings, randing from a small 8 person event to over 300 guests</p>  
+      </div>
+
+    </div>
     </section>
   </main>
 
