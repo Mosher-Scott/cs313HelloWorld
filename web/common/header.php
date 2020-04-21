e31d4207946e4337e0931d7f80f4662defb10072
Scott Mosher <scott@scottmosherphotography.com>
2020-04-20 19:37:14 -0600

    Tweaked CSS

--- web/common/header.php
+++ web/common/header.php
@@ -12,10 +12,20 @@
 
 <body>
     <header>
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-sm">
+                <div class="col">
                     <h2>CS 313 - Backend Development</h2>
+                </div>
+                <div class="col">
+                    <nav>
+                        <label for="toggle">Menu</label>
+                        <input id="toggle" type="checkbox">
+                        <ul id="menu">
+                            <li class=" btn btn-primary"><a href="../index.php">Home</a></li>
+                            <li class=" btn btn-primary"><a href="../pages/assignments.php">Assignments</a></li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </div>
