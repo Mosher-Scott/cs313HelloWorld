32cac086a00cb7805d2b7aa9188efa531af5f59d
Scott Mosher <scott@scottmosherphotography.com>
2020-04-26 06:30:28 -0600

    Added w02 assignment to Assignment page

---
+++ web/pages/w02assignment.php
@@ -0,0 +1,59 @@
+<?php
+  @include_once('common/header.php');
+?>
+  <main class="rounded-corners">
+    <section>
+      <div>
+        <h1>TITLE</h1>
+        <div class="bluebar">
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+               <p>Left Text</p>
+            </div>
+            <div class="col-md-6">
+            <p>Right Text</p>
+            </div>
+        </div> 
+      </div>
+
+    <div class="bluebar">
+    </div>
+      
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+            <h3>TITLE</h3>
+        </div>    
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <p>Left Text</p>
+        </div>
+        <div class="col-md-6">
+          <p>Middle Text</p>
+        </div>
+        <div class="col-md-6">
+          <div class="img-rounded">
+          <p>Right Image</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="bluebar">
+    </div>
+
+    
+  </section>
+
+</main>
+
+<?php 
+  @include_once('common/footer.php');
+?>
