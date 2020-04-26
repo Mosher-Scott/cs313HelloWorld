fffa23c6d940a44bdb379b4ab50374f868512e7f
Scott Mosher <scott@scottmosherphotography.com>
2020-04-26 07:32:21 -0600

    Testing getting user input & changing div color

--- web/common/header.php
+++ web/common/header.php
@@ -8,8 +8,9 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/main.css">
 
-        <script src="../javascript/javascript.js"></script>
+        <script src="../scripts/javascript.js"></script>
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="../scripts/jquery.js"></script>
         <title>CS 313 - Home</title>
     </head>
 
