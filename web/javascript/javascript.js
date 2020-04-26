fe65a80ca3a92108ca2cc208a5d44e9756f9114a
Scott Mosher <scott@scottmosherphotography.com>
2020-04-26 07:01:36 -0600

    Testing getting user input & changing div color

--- web/javascript/javascript.js
+++ web/javascript/javascript.js
@@ -9,8 +9,8 @@ function displayAlertMessage() {
 // Gets user input from a box, and changes the background color of the first div
 function changeDivColor() {
     // First get the value in the user input box
-    var color = document.getElementById("colorInputBox");
-    console.log(color);
+    var color = document.getElementById("colorInputBox").innerHTML;
+    console.log("Color: " . color);
 
     // Now change the background color of the element with the ID of firstDiv
 
