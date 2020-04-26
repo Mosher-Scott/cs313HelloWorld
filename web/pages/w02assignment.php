4592f98b30b91529013aa9497c8d6bdb83ab4faa
Scott Mosher <scott@scottmosherphotography.com>
2020-04-26 06:46:59 -0600

    Added page instructions

--- web/pages/w02assignment.php
+++ web/pages/w02assignment.php
@@ -15,8 +15,20 @@
       <div class="container-fluid">
         <div class="row">
             <div class="col-md-6">
-               <p>Click the button to display an alert message</p>
-               <button id="alertButton" onclick="displayAlertMessage()">Click Me!</button>
+              <p>This assignment has a few parts:</p>
+              <ol>
+                <li>Add a button for the user to click to display an alert message</li>
+                <li>Add classes/id tags to divs</li>
+                <li>Use CSS to bold some text when the user hovers over the text</li>
+                <li>Allow user to specify custom colors for the first div in the document</li>
+              </ol>
+
+              <p>Optional requirements</p>
+              <ol>
+                <li>Allow the user to change the background color, but use jQuery instead of Javascript</li>
+                <li>Toggle visbility of the 3rd div, and fade visiblity in/out</li>
+                <li>Use Bootstrap</li>
+              </ol>
             </div>
         </div> 
       </div>
@@ -33,7 +45,8 @@
 
       <div class="row">
         <div class="col-md-6">
-          <p>Left Text</p>
+          <p>Click the button to display an alert message:</p>
+          <button id="alertButton" onclick="displayAlertMessage()">Click Me!</button>
         </div>
         <div class="col-md-6">
           <p>Middle Text</p>
