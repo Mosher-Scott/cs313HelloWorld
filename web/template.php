c3159c676ec99eb5c7cb8b51c12f29fc1253941c
Scott Mosher <scott@scottmosherphotography.com>
2020-04-26 06:18:37 -0600

    Added template file

---
+++ web/template.php
@@ -0,0 +1,59 @@
+<?php
+  @include_once('common/header.php');
+?>
+  <main class="rounded-corners">
+    <section>
+      <div>
+        <h1>TITLE</h1>
+        <div class="bluebar">
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+               
+            </div>
+            <div class="col-md-6">
+                
+            </div>
+        </div> 
+      </div>
+
+    <div class="bluebar">
+    </div>
+      
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+            <h3>TITLE</h3>
+        </div>    
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+
+        </div>
+        <div class="col-md-6">
+         
+        </div>
+        <div class="col-md-6">
+          <div class="img-rounded">
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="bluebar">
+    </div>
+
+    
+  </section>
+
+</main>
+
+<?php 
+  @include_once('common/footer.php');
+?>
