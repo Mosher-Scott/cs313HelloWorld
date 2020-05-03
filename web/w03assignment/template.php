9161ad804e0f97abbad0ac3ae700440af46fa17b
Mosher-Scott <jorgemonkey81@byui.edu>
2020-05-03 16:47:04 -0600

    this

---
+++ web/w03assignment/template.php
@@ -0,0 +1,59 @@
+<?php
+    @include_once('../common/header.php');
+    @include_once('../common/nav.php');
+?>
+  <main class="rounded-corners">
+    <section>
+      <div>
+        <h1>TITLE</h1>
+        <div class="bluebar">
+        </div>
+      </div>
+    </section>
+
+    <section class="products">
+      <div class="container centered">
+        <div class="row">
+          <div class="col-sm">
+            Product 1
+          </div>
+          <div class="col-sm">
+          Product 2
+          </div>
+          <div class="col-sm">
+            Product 3
+          </div>
+          <div class="col-sm">
+            Product 4
+          </div>
+        </div>
+      </div>
+
+      <div class="container centered">
+        <div class="row">
+          <div class="col-sm">
+            Product 5
+          </div>
+          <div class="col-sm">
+          Product 6
+          </div>
+          <div class="col-sm">
+            Product 7
+          </div>
+          <div class="col-sm">
+            Product 8
+          </div>
+        </div>
+      </div>
+
+      <div class="bluebar">
+      </div>
+
+    
+  </section>
+
+</main>
+
+<?php 
+  @include_once('common/footer.php');
+?>
