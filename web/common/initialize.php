e15d91f973c2fc7fa258ab095de58964730a595d
Scott Mosher <scott@scottmosherphotography.com>
2020-05-06 18:47:29 -0600

    Finished team assignment

--- web/common/initialize.php
+++ web/common/initialize.php
@@ -17,7 +17,7 @@
         if($script_path[0] != '/') {
             $script_path = '/' . $script_path;
         }
-            echo $script_path;
+            echo "Script_path: " . $script_path;
        return WWW_ROOT . $script_path;
     }
 
