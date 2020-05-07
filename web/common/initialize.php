b728009571c9e6c9b4ea00f28fb5b399c095b5e3
Scott Mosher <scott@scottmosherphotography.com>
2020-05-06 18:42:04 -0600

    Finished team assignment

--- web/common/initialize.php
+++ web/common/initialize.php
@@ -4,7 +4,7 @@
     define("PROJECT_PATH", dirname(PRIVATE_PATH));
 
      // * Can dynamically find everything in URL up to "/acme"
-     $public_end = strpos($_SERVER['SCRIPT_NAME'], '/web') + 5; // Decides that is where the document root is
+     $public_end = strpos($_SERVER['SCRIPT_NAME'], '/web') + 6; // Decides that is where the document root is
      $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
      define("WWW_ROOT", $doc_root);
 
