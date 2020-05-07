2291e986374a88acf6503784648909032fdffcb3
Scott Mosher <scott@scottmosherphotography.com>
2020-05-06 19:11:49 -0600

    Fixed errors

--- web/common/initialize.php
+++ web/common/initialize.php
@@ -4,31 +4,31 @@
     define("PROJECT_PATH", dirname(PRIVATE_PATH));
 
      // * Can dynamically find everything in URL up to "/acme"
-     $public_end = strpos($_SERVER['SCRIPT_NAME'], '/web') + 7; // Decides that is where the document root is  Use 4 for local
+     $public_end = strpos($_SERVER['SCRIPT_NAME'], '/web') + 4; // Decides that is where the document root is  Use 4 for local
      $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
      define("WWW_ROOT", $doc_root);
 
     //echo $doc_root;
 
     // Create the links automatically when on the local server
-    // function urlPath($script_path) {
-    //     // Add the leading / if not present
-
-    //     if($script_path[0] != '/') {
-    //         $script_path = '/' . $script_path;
-    //     }
-        
-    //    return WWW_ROOT . $script_path;
-    // }
-
-    // For live site
     function urlPath($script_path) {
         // Add the leading / if not present
 
         if($script_path[0] != '/') {
             $script_path = '/' . $script_path;
         }
-            // echo $script_path;
-       return $script_path;
+        
+       return WWW_ROOT . $script_path;
     }
+
+    // For live site
+    // function urlPath($script_path) {
+    //     // Add the leading / if not present
+
+    //     if($script_path[0] != '/') {
+    //         $script_path = '/' . $script_path;
+    //     }
+    //         // echo $script_path;
+    //    return $script_path;
+    // }
 ?>
--- web/pages/w03Team-Assignment/teamAssignment.php
+++ web/pages/w03Team-Assignment/teamAssignment.php
@@ -1,6 +1,13 @@
 <?php
     @include_once('../../common/header.php');
     @include_once('../../common/nav.php');
+
+    $majors = array(
+      "CS" => "Computer Science",
+      "WebDev" => "Web Design and Development",
+      "CIT" => "Computer Information Technology",
+      "CE" => "Computer Engineering"
+    );
 ?>
   <main class="rounded-corners">
     <section>
@@ -12,16 +19,15 @@
     </section>
 
     <section>
+    <h3><b>Please fill out all fields</b></h3>
     <form action="<?php echo urlPath('/pages/w03Team-Assignment/formCode.php'); ?>" method="post">
         <!--User Name and Email-->
         <div>
-        <div>
-            <p><b>Please fill out all fields</b></p>
-          </div>
-
           <div class="form-group">
-            <label for="userName">Name: <input type="text" class="form-control" name="userName"></label><br>
-            <label for="userEmail">Email: <input type="text" class="form-control" name="userEmail"></label><br>
+            <label for="userName">Name:</label>
+            <input type="text" class="form-control" name="userName" id="userName">
+            <label for="userEmail">Email:</label>
+            <input type="text" class="form-control" name="userEmail" id="userEmail">
           </div>
         </div>
 
@@ -31,23 +37,16 @@
           <div>
             <p><b>What is your major?</b></p>
           </div>
-        
-          <div class="form-check">
-            <input type="radio" id="CS" class="form-check-input" name="major" value="Computer Science">
-            <label for="CS" class="form-check-label">Computer Science</label>
-          </div>
-          <div class="form-check">
-            <input type="radio" id="WebDev" class="form-check-input" name="major" value="Web Design and Development">
-            <label for="WebDev" class="form-check-label">Web Design and Development</label>
-          </div>
-          <div class="form-check">
-            <input type="radio" id="CIT" class="form-check-input" name="major" value="Computer Information Technology">
-            <label for="CIT" class="form-check-label">Computer Information Technology</label>
-          </div>
-          <div class="form-check">
-            <input type="radio" id="CE" class="form-check-input" name="major" value="Computer Engineering">
-            <label for="CE" class="form-check-label">Computer Engineering</label>
-          </div>
+
+          <?php
+            foreach($majors as $key => $value) {
+              echo "<div class='form-check'>";
+              echo "<input type='radio' id='{$key}' class='form-check-input' name='major' value='{$value}'>";
+              echo "<label for='{$key}' class='form-check-label'>{$value}</label>";
+              echo "</div>";
+            }
+          ?>
+
         </div>
 
         <hr>
@@ -66,36 +65,42 @@
             <input type="checkbox" class="form-check-input" name="places[]" id="southAmerica" value="1">
             <label for="southAmerica" class="form-check-label">South America</label>
           </div>
+
           <div class="form-check">
             <input type="checkbox" class="form-check-input" name="places[]" id="europe" value="2">
             <label for="europe" class="form-check-label">Europe</label>
           </div>
+
           <div class="form-check">
             <input type="checkbox" class="form-check-input" name="places[]" id="asia" value="3">
-            <label for="Asia" class="form-check-label">Asia</label>
+            <label for="asia" class="form-check-label">Asia</label>
           </div>
+
           <div class="form-check">
             <input type="checkbox" class="form-check-input" name="places[]" id="australia" value="4">
             <label for="australia" class="form-check-label">Australia</label>
           </div>
+
           <div class="form-check">
             <input type="checkbox" class="form-check-input" name="places[]" id="africa" value="5">
             <label for="africa" class="form-check-label">Africa</label>
           </div>
+
           <div class="form-check">
             <input type="checkbox" class="form-check-input" name="places[]" id="antarctica" value="6">
-            <label for="Antarctica" class="form-check-label">Antarctica</label>
+            <label for="antarctica" class="form-check-label">Antarctica</label>
           </div>
+          
         </div>
 
-        <div>
         <hr>
         <div>
           <div>
             <p><b>Comments?</b></p>
           </div>
           <!--User thoughts-->
-          <label for="comments"><textarea name="comments" class="form-control" cols="20" rows="4">Enter Your Comments...</textarea></label><br>
+          <label for="comments">Comments</label>
+          <textarea name="comments" class="form-control" cols="20" rows="4" id="comments">Enter Your Comments...</textarea>
           <input type="submit" class="btn btn-primary" name="submit" value="Submit">
         </div>
 
@@ -108,3 +113,21 @@
 <?php 
   @include_once('common/footer.php');
 ?>
+
+        
+          <!-- <div class="form-check">
+            <input type="radio" id="CS" class="form-check-input" name="major" value="Computer Science">
+            <label for="CS" class="form-check-label">Computer Science</label>
+          </div>
+          <div class="form-check">
+            <input type="radio" id="WebDev" class="form-check-input" name="major" value="Web Design and Development">
+            <label for="WebDev" class="form-check-label">Web Design and Development</label>
+          </div>
+          <div class="form-check">
+            <input type="radio" id="CIT" class="form-check-input" name="major" value="Computer Information Technology">
+            <label for="CIT" class="form-check-label">Computer Information Technology</label>
+          </div>
+          <div class="form-check">
+            <input type="radio" id="CE" class="form-check-input" name="major" value="Computer Engineering">
+            <label for="CE" class="form-check-label">Computer Engineering</label>
+          </div> -->
