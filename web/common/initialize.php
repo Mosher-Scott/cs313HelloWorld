9be62c13ce62fbd529f62f85cd1104ca20a50962
Scott Mosher <scott@scottmosherphotography.com>
2020-05-23 10:34:11 -0600

    stuff

--- web/common/initialize.php
+++ web/common/initialize.php
@@ -29,7 +29,9 @@
         }
     }
 
-    @require_once(__DIR__ . '/common/phpMethods.php');
+    $basePath =  urlPath("/common/phpMethods.php");
+
+    @require_once($basePath);
     @require_once('dbconnection.php');
     //@require_once(urlPath('common/dbconnection.php'));
     //@require_once('../model/products-model.php');
